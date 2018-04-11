e86f835fec94331d30133acfaca00440a40514b2
Sundir Talari <sundirtalari@sundirs-macbook-air.local>
2018-04-11 11:19:42 +0530

    Added Successfully

---
+++ MobileProject/MobileModel.swift
@@ -0,0 +1,18 @@
+//
+//  MobileModel.swift
+//  MobileProject
+//
+//  Created by Sundir Talari on 30/03/18.
+//  Copyright © 2018 Sundir Talari. All rights reserved.
+//
+
+import UIKit
+
+class MobileModel: NSObject {
+    
+    var name = ""
+    var number = 0
+    var model = ""
+    var madeIn = ""
+
+}
---
+++ MobileProject/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MobileProject
+//
+//  Created by Sundir Talari on 30/03/18.
+//  Copyright © 2018 Sundir Talari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+  var myArr = [MobileModel]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var mobileModelObj = MobileModel()
+        mobileModelObj.name = "Cellkon"
+        mobileModelObj.number = 789
+        mobileModelObj.model = "c199"
+        mobileModelObj.madeIn = "India"
+        
+        for i in 1...4 {
+            
+            myArr.append(mobileModelObj)
+            
+        }
+        print(myArr.count)
+        print("mobile name is \(mobileModelObj.name)")
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
